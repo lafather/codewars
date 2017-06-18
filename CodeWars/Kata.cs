c3433cb56ca17520917d2251547a8169914c00c6
Vitalii Feilo <maidensvanquisher@gmail.com>
2017-06-18 12:21:25 +0300

    Added a solution for CodeWars exercises.

---
+++ CodeWars/Kata.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class Kata
+    {
+        public static string Disemvowel(string str)
+        {
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            return string.Join(string.Empty, str.Split(vowels));
+        }
+
+        public static int FindParityOutlier(int[] integers)
+        {
+            if (integers[0] % 2 != integers[1] % 2)
+            {
+                return integers[2] % 2 == integers[0] % 2 ? integers[1] : integers[0];
+            }
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                var value = integers[i];
+
+                integers[i] &= 1;
+
+                if (i > 0 && (integers[i] + integers[i-1]) % 2 == 1)
+                {
+                    return value;
+                }
+            }
+
+            return integers[0];
+        }
+
+        public static bool XsAndOs(string input)
+        {
+            var builder = new StringBuilder(input);
+            var inputWithoutXs = builder.Replace("x", string.Empty).Replace("X", string.Empty);
+            var lengthWithoutXs = inputWithoutXs.Length;
+            var inputWithoutXsAndOs = inputWithoutXs.Replace("o", string.Empty).Replace("O", string.Empty);
+            var lengthWithoutXsAndOs = inputWithoutXsAndOs.Length;
+
+            return input.Length + lengthWithoutXsAndOs == 2 * lengthWithoutXs;
+        }
+
+        public static string GetRectangleString(int width, int height)
+        {
+            var result = new StringBuilder();
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int column = 0; column < width; column++)
+                {
+                    if (row == 0 || row == height - 1 || column == 0 || column == width - 1)
+                    {
+                        result.Append('*');                        
+                    }
+                    else
+                    {
+                        result.Append(' ');
+                    }
+                }
+
+                result.Append("\r\n");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ CodeWarsTests/KataTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void DisemvowelTest()
+        {
+            Assert.AreEqual(Kata.Disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!");
+        }
+
+        [Test]
+        public static void FindParityOutlierTest1()
+        {
+            int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+            Assert.IsTrue(3 == Kata.FindParityOutlier(exampleTest1));
+        }
+
+        [Test]
+        public static void FindParityOutlierTest2()
+        {
+            int[] exampleTest2 = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
+            Assert.IsTrue(206847684 == Kata.FindParityOutlier(exampleTest2));
+        }
+
+        [Test]
+        public static void FindParityOutlierTest3()
+        {
+            int[] exampleTest3 = { int.MaxValue, 0, 1 };
+            Assert.IsTrue(0 == Kata.FindParityOutlier(exampleTest3));
+        }
+
+        [Test]
+        public void XsAndOsTests()
+        {
+            Assert.AreEqual(true, Kata.XsAndOs("xo"));
+            Assert.AreEqual(true, Kata.XsAndOs("xxOo"));
+            Assert.AreEqual(false, Kata.XsAndOs("xxxm"));
+            Assert.AreEqual(false, Kata.XsAndOs("Oo"));
+            Assert.AreEqual(false, Kata.XsAndOs("ooom"));
+        }
+
+        [Test]
+        public void GetRectangleString_3x3()
+        {
+            string rectangle = Kata.GetRectangleString(3, 3);
+            string expected = "***\r\n" +
+                              "* *\r\n" +
+                              "***\r\n";
+            Assert.AreEqual(expected, rectangle);
+        }
+    }
+}
