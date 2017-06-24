5aa2fd27a97e15c099d66e3153319c395c108c48
Vitalii Feilo <maidensvanquisher@gmail.com>
2017-06-24 20:31:55 +0300

    The 6th kyu.

--- CodeWars/Kata.cs
+++ CodeWars/Kata.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeWars
@@ -71,5 +73,172 @@ namespace CodeWars
 
             return result.ToString();
         }
+
+        public static int BinaryArrayToNumber(int[] binaryArray)
+        {
+            var result = 0;
+            var length = binaryArray.Length;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                result += binaryArray[i] * (int)Math.Pow(2, length - i - 1);
+            }
+
+            return result;
+        }
+
+        public static int GetLengthOfMissingArray(object[][] arrayOfArrays)
+        {
+            if (arrayOfArrays == null || !arrayOfArrays.Any() || arrayOfArrays.Any(x => x == null || !x.Any()))
+            {
+                return 0;
+            }
+
+            var countOfArrays = arrayOfArrays.Count();
+            var minLength = arrayOfArrays.Min(x => x.Length);
+            var maxLength = arrayOfArrays.Max(y => y.Length);
+            var expectedSumOfLengths = (minLength + maxLength) * (countOfArrays + 1) / 2;
+            var actualSumOfLengths = arrayOfArrays.Sum(arr => arr.Length);
+
+            return expectedSumOfLengths - actualSumOfLengths;
+        }
+
+        public static string RevRot(string str, int sz)
+        {
+            return 
+                string.IsNullOrEmpty(str) || sz <= 0 || sz > str.Length 
+                    ? string.Empty 
+                    : string.Join(
+                        string.Empty, 
+                        str.GetChunks(sz).Select(RotateOrReverse));
+        }
+
+        public static string ToCamelCase(string str)
+        {            
+            var words = str.Split('-', '_');
+
+            return words.Skip(1).Aggregate(new StringBuilder(words.First()), (x, y) => x.Append(y.Capitalize())).ToString();
+        }
+
+        public static int BouncingBall(double h, double bounce, double window)
+        {
+            return 
+                h > 0 && bounce > 0 && bounce < 1 && window < h 
+                    ? 1 + 2 * (int)Math.Ceiling(Math.Log(window / h) / Math.Log(bounce) - 1) 
+                    : -1;
+        }
+
+        public static bool IsMerge(string s, string part1, string part2)
+        {
+            var wordStatistics = s.GetLettersStatistics();
+            var partsStatistics = part1.GetLettersStatistics().Merge(part2.GetLettersStatistics());
+            var primaryValidation = !wordStatistics.Any(x => !partsStatistics.ContainsKey(x.Key) || x.Value != partsStatistics[x.Key]);
+
+            return primaryValidation && part1.IsOrderOfLettersCorrect(s) && part2.IsOrderOfLettersCorrect(s);
+        }
+
+        #region String extensions
+        private static IEnumerable<string> GetChunks(this string str, int size)
+        {
+            var result = new List<string>();
+
+            for (int i = 0; i < str.Length / size; i++)
+            {
+                result.Add(str.Substring(i * size, size));
+            }
+
+            return result;
+        }
+
+        private static int GetSumOfCubes(this string input)
+        {
+            return input.Sum(x => (int)Math.Pow(int.Parse(x.ToString()), 3));
+        }
+
+        private static string RotateOrReverse(string chunk)
+        {
+            return (chunk.GetSumOfCubes() & 1) == 0 ? chunk.Reverse() : chunk.Rotate(true, 1);
+        }
+
+        private static IEnumerable<string> GraphemeClusters(this string s)
+        {
+            var enumerator = StringInfo.GetTextElementEnumerator(s);
+
+            while (enumerator.MoveNext())
+            {
+                yield return (string)enumerator.Current;
+            }
+        }
+
+        private static string Reverse(this string s)
+        {
+            return string.Join(string.Empty, s.GraphemeClusters().Reverse().ToArray());
+        }
+
+        private static string Rotate(this string s, bool left, int positions)
+        {
+            var insertionUndex = left ? s.Length - positions : 0;
+            var subStringStartIndex = left ? 0 : s.Length - positions;
+            var subString = s.Substring(subStringStartIndex, positions);
+
+            return new StringBuilder(s).Remove(subStringStartIndex, positions).Insert(insertionUndex, subString).ToString();
+        }
+
+        private static string Capitalize(this string input)
+        {
+            return 
+                new StringBuilder(input)
+                    .Replace(
+                        input.First(), 
+                        input.First().ToString().ToUpper().ToCharArray().First(), 
+                        0, 
+                        1)
+                    .ToString();
+        }
+
+        private static IDictionary<char, int> GetLettersStatistics(this string str)
+        {
+            return str.GroupBy(x => x).ToDictionary(y => y.Key, z => z.Count());
+        }
+
+        private static bool IsOrderOfLettersCorrect(this string input, string str)
+        {
+            var previousIndex = 0;
+            var newIndex = 0;
+
+            foreach (var letter in input)
+            {
+                newIndex = str.IndexOf(letter, previousIndex);
+
+                if (newIndex == -1)
+                {
+                    return false;
+                }
+
+                previousIndex = ++newIndex;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Dictionary extensions
+        private static IDictionary<T, int> Merge<T>(this IDictionary<T, int> dict1, IDictionary<T, int> dict2)
+        {
+            var result = new Dictionary<T, int>();
+
+            foreach (var pair in dict1.Where(x => dict2.ContainsKey(x.Key)))
+            {
+                result.Add(pair.Key, pair.Value + dict2[pair.Key]);
+            }
+
+            foreach (var pair in dict1.Where(x => !dict2.ContainsKey(x.Key)).Concat(dict2.Where(x => !dict1.ContainsKey(x.Key))))
+            {
+                result.Add(pair.Key, pair.Value);
+            }
+
+            return result;
+        }
+        #endregion
     }
 }
--- CodeWarsTests/KataTests.cs
+++ CodeWarsTests/KataTests.cs
@@ -57,5 +57,65 @@ namespace CodeWars.Tests
                               "***\r\n";
             Assert.AreEqual(expected, rectangle);
         }
+
+        int[] Test1 = new int[] { 0, 0, 0, 0 };
+        int[] Test2 = new int[] { 1, 1, 1, 1 };
+        int[] Test3 = new int[] { 0, 1, 1, 0 };
+        int[] Test4 = new int[] { 0, 1, 0, 1 };
+        [Test]
+        public void BinaryArrayToNumberTest()
+        {
+            var test1 = new int[] { 0, 0, 0, 0 };
+            var test2 = new int[] { 1, 1, 1, 1 };
+            var test3 = new int[] { 0, 1, 1, 0 };
+            var test4 = new int[] { 0, 1, 0, 1 };
+
+            Assert.AreEqual(0, Kata.BinaryArrayToNumber(test1));
+            Assert.AreEqual(15, Kata.BinaryArrayToNumber(test2));
+            Assert.AreEqual(6, Kata.BinaryArrayToNumber(test3));
+            Assert.AreEqual(5, Kata.BinaryArrayToNumber(test4));
+        }
+
+        [Test]
+        public void GetLengthOfMissingArrayTests()
+        {
+            Assert.AreEqual(3, Kata.GetLengthOfMissingArray(new object[][] { new object[] { 1, 2 }, new object[] { 4, 5, 1, 1 }, new object[] { 1 }, new object[] { 5, 6, 7, 8, 9 } }));
+            Assert.AreEqual(2, Kata.GetLengthOfMissingArray(new object[][] { new object[] { 5, 2, 9 }, new object[] { 4, 5, 1, 1 }, new object[] { 1 }, new object[] { 5, 6, 7, 8, 9 } }));
+            Assert.AreEqual(2, Kata.GetLengthOfMissingArray(new object[][] { new object[] { null }, new object[] { null, null, null } }));
+            Assert.AreEqual(5, Kata.GetLengthOfMissingArray(new object[][] { new object[] { 'a', 'a', 'a' }, new object[] { 'a', 'a' }, new object[] { 'a', 'a', 'a', 'a' }, new object[] { 'a' }, new object[] { 'a', 'a', 'a', 'a', 'a', 'a' } }));
+
+            Assert.AreEqual(0, Kata.GetLengthOfMissingArray(new object[][] { }));
+        }
+
+        [Test]
+        public void RevRotTests()
+        {
+            StringAssert.IsMatch(string.Empty, Kata.RevRot("1234", 0));
+            StringAssert.IsMatch(string.Empty, Kata.RevRot("1234", 5));
+            StringAssert.IsMatch(string.Empty, Kata.RevRot(string.Empty, 0));
+            StringAssert.IsMatch("330479108928157", Kata.RevRot("733049910872815764", 5));
+        }
+
+        [Test]
+        public void ToCamelCaseTests()
+        {
+            Assert.AreEqual("theStealthWarrior", Kata.ToCamelCase("the_stealth_warrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+            Assert.AreEqual("TheStealthWarrior", Kata.ToCamelCase("The-Stealth-Warrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+        }
+
+        [Test]
+        public void BouncingBallTests()
+        {
+            Assert.AreEqual(3, Kata.BouncingBall(3.0, 0.66, 1.5));
+            Assert.AreEqual(15, Kata.BouncingBall(30.0, 0.66, 1.5));
+        }
+
+        [Test]
+        public void IsMergeTests()
+        {
+            Assert.IsTrue(Kata.IsMerge("codewars", "code", "wars"), "codewars can be created from code and wars");
+            Assert.IsTrue(Kata.IsMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
+            Assert.IsFalse(Kata.IsMerge("codewars", "cod", "wars"), "Codewars are not codwars");
+        }
     }
 }
